50c19f2e9d954fdcb1d4da5261a7f5160f2106ff
rslavin <rocky.slavin@gmail.com>
2023-09-28 18:10:13 -0500

    refactoring and todos

--- gpt_client.py
+++ gpt_client.py
@@ -8,9 +8,13 @@ from datetime import datetime
 from timeout_function_decorator.timeout_decorator import timeout
 from tiktoken import encoding_for_model
 
-MAX_MODEL_TOKENS = 8192  # max tokens the model can handle
-MODEL = "gpt-4"
-MAX_RESPONSE_TOKENS = 150  # max tokens in response
+# max tokens the model can handle - lowering this can reduce api cost since the entire conversation is sent
+# with each request
+# TODO pay attention to short replies that occur due to long conversations: https://platform.openai.com/docs/guides/gpt/managing-tokens
+MAX_MODEL_TOKENS = 8000  # 8192
+MODEL = "gpt-3.5-turbo-16k"
+#MODEL = "gpt-4"
+MAX_RESPONSE_TOKENS = 200  # max tokens in response
 HISTORY_DIR = "personas"
 DIRECTIVES_PATH = "config/gpt_directives.json"
 
@@ -23,22 +27,6 @@ def count_tokens(text) -> int:
 def add_timestamp(text) -> str:
     timestamp = datetime.now().strftime("[%B %-d, %Y %-I:%M:%S%p]")
     return f"{timestamp} {text}"
-
-
-def load_conversation(file_path) -> []:
-    conversation = []
-    try:
-        with open(file_path, "rb") as f:
-            while True:
-                try:
-                    conversation.append(pickle.load(f))
-                except EOFError:
-                    break
-    except Exception as e:
-        print(f"The following exception occurred when trying to load {file_path}: {e}")
-        if not conversation:
-            print("The conversation was not loaded. A new conversation has been created.")
-    return conversation
 
 
 def get_system_directives():
@@ -61,24 +49,40 @@ class GptClient:
         # load from disk
         dir_path = os.path.dirname(os.path.realpath(__file__))
         conv_file = f"{self.persona.name}_DEBUG.pkl" if os.getenv("APP_ENV") == "LOCAL" else f"{self.persona.name}.pkl"
-        self.pkl_file = os.path.join(dir_path, HISTORY_DIR, conv_file)
-        self.conversation = load_conversation(self.pkl_file)
-
-        if self.conversation:
-            print(f"{self.persona.name}'s conversation history successfully loaded.")
-            pprint(self.conversation)
 
         self.system_msg = {
             "role": "system",
             "content": " ".join(persona.personality_rules) + "\n\n" + " ".join(get_system_directives())
         }
         self.total_tokens = count_tokens(self.system_msg['content'])
-        # make room in case the loaded conversation is too long
+        self.pkl_file = os.path.join(dir_path, HISTORY_DIR, conv_file)
+        self.conversation = []
+        self.load_conversation()
         self.make_room()
+        if self.conversation:
+            pprint(self.conversation)
+
+    def load_conversation(self):
+        try:
+            with open(self.pkl_file, "rb") as f:
+                while True:
+                    try:
+                        msg = pickle.load(f)
+                        pprint(msg)
+                        self.append_message(msg['role'], msg['content'])
+                    except EOFError:
+                        break
+            self.make_room()
+        except Exception as e:
+            print(f"The following exception occurred when trying to load {self.pkl_file}: {e}")
+        if self.conversation:
+            print(f"{self.persona.name}'s conversation history successfully loaded.")
+        else:
+            print("The conversation was not loaded. A new conversation has been created.")
 
     @timeout(15)
     def get_response(self, message):
-        self.append_message("user", message)
+        self.append_message("user", message, to_disk=True)
         self.make_room()
 
         chat = openai.ChatCompletion.create(
@@ -88,14 +92,21 @@ class GptClient:
             max_tokens=MAX_RESPONSE_TOKENS
         )
         response = chat.choices[0].message.content
-        self.append_message("assistant", response)
+        self.append_message("assistant", response, to_disk=True)
 
         return response
+
+    def get_total_token_count(self):
+        total = count_tokens(self.system_msg['content'])
+        for message in self.conversation:
+            if 'content' in message:
+                total += count_tokens(message['content'])
+        return total
 
     @timeout(8)
     def get_response_generator(self, message):
         # TODO make modifications directly to the message to reinforce certain rules
-        self.append_message("user", add_timestamp(message))
+        self.append_message("user", add_timestamp(message), to_disk=True)
         self.make_room()
 
         sentence_buffer = ""
@@ -113,8 +124,7 @@ class GptClient:
                 sentence_buffer += content  # current sentence
 
                 # check if the buffer contains a full sentence
-                # TODO stop this from catching enumerated lists \d+\.
-                if re.search(r"[^\s.]{2,}[\.\?!\n]", sentence_buffer):
+                if re.search(r"[^\s.\d]{2,}[\.\?!\n]", sentence_buffer):
                     sentence_buffer = re.sub(r"^\[.+\] ", '', sentence_buffer)
                     response += sentence_buffer
                     print(f'\t"{sentence_buffer.strip()}"')
@@ -128,8 +138,9 @@ class GptClient:
                 print(f'\t"{sentence_buffer.strip()}"')
                 response += sentence_buffer
                 yield sentence_buffer
-
-        self.append_message("assistant", response)
+        # TODO if the choices[0].get("finish_reason") is "length", have the system let the user know they've reached
+        # TODO the directed maximum token limit and ask if they'd like the system to continue. (will have to allow "yes" and "no" through preprocessing)
+        self.append_message("assistant", response, to_disk=True)
         yield None
 
     def make_room(self):
@@ -144,7 +155,7 @@ class GptClient:
             self.total_tokens -= removed_token_count
             print(f"Pruning history to make room... {removed_token_count} tokens freed.")
 
-    def append_message(self, role, message):
+    def append_message(self, role, message, to_disk=False):
         message_tokens = count_tokens(message)
         print(f"Message tokens: {message_tokens}")
         self.total_tokens += message_tokens
@@ -155,11 +166,12 @@ class GptClient:
             "content": message
         }
         self.conversation.append(message)
-        try:
-            with open(self.pkl_file, "ab+") as f:
-                pickle.dump(message, f)
-        except Exception as e:
-            print(f"Unable to write to {self.pkl_file}: {e}")
+        if to_disk:
+            try:
+                with open(self.pkl_file, "ab+") as f:
+                    pickle.dump(message, f)
+            except Exception as e:
+                print(f"Unable to write to {self.pkl_file}: {e}")
 
     def get_conversation(self):
         conversation = self.conversation[:-3] + [self.system_msg] + self.conversation[-2:]
